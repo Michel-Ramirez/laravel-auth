820b1d7b441b8b4dac6a1f21c6cfe1069fee74d2
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-09-05 16:19:07 +0200

    add validation and fix migration

--- database/migrations/2023_08_31_155126_create_projects_table.php
+++ database/migrations/2023_08_31_155126_create_projects_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title', 50)->unique();
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('description');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
